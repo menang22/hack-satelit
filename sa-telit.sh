b273aa2a8b22ea248ecec7ed40bd05da4589ef13
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-02 15:46:43 +0700

    Create sa-telit.sh

---
+++ sa-telit.sh
@@ -0,0 +1,181 @@
+#!bin/sh
+bgp='\033[0;107m'
+r='\033[1;31m'
+h='\033[1;32m'
+k='\033[1;33m'
+b='\033[1;34m'
+u='\033[1;35m'
+c='\033[1;36m'
+p='\033[1;37m'
+clear='\033[0m'
+clear
+echo "${p}pesan${r}: ${k}PROGRAM INI HANYA PROGRAM SEDERHANA YANG 
+NOVAL(SAYA) DESIGN TIDAK BENAR BENAR BERFUNGSI
+UNTUK HACK SATELIT. JADI JANGAN BAPER YA BRO!!!"
+echo "${u}"
+read -p "[?] Enter To Continue" continue
+clear
+echo " ${r}┏━╮╭━┓
+ ${r}┃┏┗┛┓┃
+ ${r}╰┓▋▋┏╯      ${h}╔═══════════════════════════════════╗
+${r}╭━┻╮╲┗━━━━╮╭╮${h}║   ${u}* ${p}Author   ${r}: ${c}Gabriel.S   ${h}║
+${r}┃▎▎┃╲╲╲╲╲╲┣━╯${h}║     ${p}Instagram${r}: ${u}@gabriel         ${h}║
+${r}╰━┳┻▅╯╲╲╲╲┃  ${h}║     ${p}Youtube  ${r}: ${u}Winner             ${h}║
+${r}  ╰━┳┓┏┳┓┏╯  ${h}╚═══════════════════════════════════╝
+${r}    ┗┻┛┗┻┛${k}\n"
+read -p "[?] Enter To Continue" continue
+clear
+echo " ${r}┏━╮╭━┓
+ ${r}┃┏┗┛┓┃
+ ${r}╰┓▋▋┏╯      
+${r}╭━┻╮╲┗━━━━╮╭╮${h}    ${u}* ${p}Author   ${r}: ${c}Gabriel.S   ${h}
+${r}┃▎▎┃╲╲╲╲╲╲┣━╯${h}      ${p}Instagram${r}: ${u}@gabriel         ${h}
+${r}╰━┳┻▅╯╲╲╲╲┃  ${h}      ${p}Youtube  ${r}: ${u}Winner             ${h}
+${r}  ╰━┳┓┏┳┓┏╯  
+${r}    ┗┻┛┗┻┛"
+echo "${b}╔════════════════════════════════════════════════╗
+${b}║     ${r}┏━┓┏━┓┏┳┓┏━┓${p}┳  ┳┏┳┓┏━┓  ${r}┳ ┳┏━┓┏━┓┳┏━       ${b}║
+${b}║     ${r}┗━┓┣━┫ ┃ ┃┫ ${p}┃  ┃ ┃ ┃┫   ${r}┣━┫┣━┫┃  ┣┻┓       ${b}║
+${b}║     ${r}┗━┛┻ ┻ ┻ ┗━┛${p}┗━┛┻ ┻ ┗━┛  ${r}┻ ┻┻ ┻┗━┛┻ ┻       ${b}║
+${b}╚════════════════════════════════════════════════╝\n"
+echo "${p}Pakai Mode Privasi? ${k}[${h}y${u}/${r}t${k}]${c}"
+read -p "[?] ▶ " proxy
+echo "\n${b}Calling Supply ${r}..."
+sleep 2
+echo "${h}Done."
+echo "\n${p}[${k}+${p}] ${h}DAFTAR SATELIT
+
+${p}║   ${p}{${b}1${p}} ${k}PALAPAC2 113E
+${p}║   ${p}{${b}2${p}} ${k}TELKOM1 108E
+${p}║   ${p}{${b}3${p}} ${k}ASIASAT3S 105E5
+${p}║   ${p}{${b}4${p}} ${k}MEASAT2 100E5
+${p}║   ${p}{${b}5${p}} ${k}ST1 88E
+${p}║   ${p}{${b}6${p}} ${k}CHINASTAR1 87E5
+${p}║   ${p}{${b}7${p}} ${k}Chinasat6B 115E
+${p}║   ${p}{${b}8${p}} ${k}THAICOM1A 120E
+${p}║   ${p}{${b}9${p}} ${k}JCSAT 3 120E
+${p}║   ${r}{0} Exit.
+${p}║"
+read -p "╚▶ " choice
+case $choice in
+1)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " PALAPAC2
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}PALAPA ${c}Berhasil 
+        Di Geser ${k}${PALAPAC2} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit
+exit
+exit
+exit
+;;
+2)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " TELKOM1
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}TELKOM ${c}Berhasil 
+        Di Geser ${k}${TELKOM1} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit1
+exit
+exit
+exit
+;;
+3)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " ASIASAT3S
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}ASIASAT ${c}Berhasil 
+        Di Geser ${k}${ASIASAT3S} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit2
+exit
+exit
+exit
+;;
+4)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " MEASAT2
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}MEASAT ${c}Berhasil 
+        Di Geser ${k}${MEASAT2} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit3
+exit
+exit
+exit
+;;
+5)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " ST1
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}ST ${c}Berhasil 
+        Di Geser ${k}${ST1} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit4
+exit
+exit
+exit
+;;
+6)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " CHINASTAR1
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}CHINASTAR ${c}Berhasil 
+        Di Geser ${k}${CHINASTAR1} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit5
+exit
+exit
+exit
+;;
+7)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " Chinasat6B
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}PChinasat ${c}Berhasil 
+        Di Geser ${k}${Chinasat6B} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit6
+exit
+exit
+exit
+;;
+8)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " THAICOM1A
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}THAICOM ${c}Berhasil 
+        Di Geser ${k}${THAICOM1A} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit7
+exit
+exit
+exit
+;;
+9)
+echo "\n${p}[${k}?${p}] ${h}GESER BERAPA LANGKAH"
+echo "    ${u}ex${r}: ${k}1, 2, 3, 4, ${h}dst\n${k}"
+read -p "    Input: " JCSAT
+echo "    ${h}Loading${r}..."
+sleep 2
+echo "    ${p}[${h}+${p}] ${c}Satelit ${k}JCSAT ${c}Berhasil 
+        Di Geser ${k}${JCSAT} ${c}Langkah${r}!\n\n"
+read -p "    [?] Enter To Exit" exit8
+exit
+exit
+exit
+;;
+*)
+echo "di isi woy"
+;;
+esac
